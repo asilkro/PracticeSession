84d3a5167445cef445cf49138fb3ed44b71eb0a1
asilkro <asilkro@me.com>
2021-01-20 17:06:04 -0800

    started adding stuff but still WIP

--- src/unit_tests.rs
+++ src/unit_tests.rs
@@ -21,6 +21,34 @@ fn word_count__emptystring_yield_count_of_zero() {
     let result=word_count(input_string);
     // Then (check result)
     assert_eq! (result,expected_result);
+
+// My make sure the filename is what we are selecting
+// Thinking: if a filename has no ' ' it should only be a single word
+// ie 'bigwillyshakescollection.txt' should return a 1
+// Therefore we want to return a result of 1
+fn wordcount__filename_is_being_read(){
+    // Given -> expected filename returns filename
+    let expected_filename;
+    //let input_filename;
+    // When
+    let file_name_result = std::fs::File;
+    // Then
+    assert_eq!(expected_filename, file_name_result);
+
+
+/*Larger Question:
+How do we distinguish an item from its contents? It seems like that std::fs::file probably has the clue.
+This is the type to 
+
+ */
+
+fn word_count__(){
+    //Placeholder
+}
+
+}
+
+
 }
 
 
