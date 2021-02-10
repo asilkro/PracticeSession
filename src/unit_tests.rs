ea575cafde117684be47908ed4ecf16d402e7358
asilkro <asilkro@me.com>
2021-02-10 11:30:35 -0800

    Working on Command Line arguments

--- src/unit_tests.rs
+++ src/unit_tests.rs
@@ -3,19 +3,56 @@ use crate::word_count;
 use std::io::Read;
 use std::fs::{read, read_to_string, File};
 use std::error::Error;
+use std::env;
 
+// #[test] // Attributes only apply to the next item, so each test
+// //sut = subject under test
+// fn word_count__empty_input_returns_zero() {
+//     // Given (setup)
+//     let expected_result = Result::Ok(0);
+//     let input_data = "".as_bytes();
+//     let sut= word_count;
+//
+//     // When (action)
+//     let result = sut(input_data);
+//
+//     //Then
+//     assert_eq!(result, expected_result);
+// }
 
-#[test] // Attributes only apply to the next item, so each test
-//sut = subject under test
-fn word_count__empty_input_returns_zero() {
-    // Given (setup)
-    let expected_result = Result::Ok(0);
+// Up next:
+// Test with 0 input ^^
+// Test with 1 input
+// Try to make a command line argument pass into Wordcount 'WC'
+
+// #[test]
+// // Given (setup)
+// fn word_count__one_input_returns_one() {
+//     let expected_result = Result::Ok(1);
+//     let input_data = "Legit".as_bytes();
+//     let sut = word_count;
+//
+// // When (action)
+//     let result = sut(input_data);
+//
+// //Then
+//     assert_eq!(result, expected_result);
+// }
+
+#[test]
+// Given (setup)
+fn word_count__command_line_input_passes() {
+    let cl_args: Vec<String> = env::args().collect();
+    let read_item = &cl_args[1];
+    let read_flags = &cl_args[2];
+
     let input_data = "".as_bytes();
-    let sut= word_count;
+    let sut = word_count;
 
-    // When (action)
-    let result = sut(input_data);
+// When (action)
+    let result = sut(input_data, flag);
 
-    //Then
-    assert_eq!(result, expected_result);
+// Then (output)
+
+
 }
